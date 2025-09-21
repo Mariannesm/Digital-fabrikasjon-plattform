fa6f6c8362dc4693bc727cd8d4ccdf83c9491a97
Mariannesm <marmyr6@hotmail.com>
2025-09-21 22:16:46 +0200

    Update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,14 +7,14 @@ function Layout({ children }) {
     <div>
       {/* Eksempel på en enkel layout-struktur */}
       <header>
-        <h1>Digital fabrikasjon</h1>
+        <h1 className='text-black font-mono'>Digital fabrikasjon</h1>
       </header>
       <main>
         {/* Her rendres den komponenten eller siden som sendes inn */}
         {children}
       </main>
       <footer>
-        <p>© 2025 Høgskolen I Østfold</p>
+        <p className='text-black font-mono'>© 2025 Høgskolen I Østfold</p>
       </footer>
     </div>
   );
--- src/pages/landingpage/LandingPage.jsx
+++ src/pages/landingpage/LandingPage.jsx
@@ -5,18 +5,18 @@ import { Route, Routes } from "react-router-dom";
 function LandingPage() {
   return (
     <>
-    <section className="w-200 h-150 flex flex-wrap -mx-2">
+    <section className="flex flex-wrap justify-center w-200 h-100 m-auto">
     <div className="w-1/2 px-2 mb-4">
-        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full ">Bruk teknologiene</button>
+        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full font-mono text-[25px] rounded-2xl drop-shadow-lg">Bruk teknologiene</button>
     </div>
     <div className="w-1/2 px-2 mb-4">
-        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full">Lær om bærekraftig bruk</button>
+        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full font-mono text-[25px] rounded-2xl drop-shadow-lg">Lær om bærekraftig bruk</button>
     </div>
     <div className="w-1/2 px-2 mb-4">
-        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full">Husk sikkerhet!</button>
+        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full font-mono text-[25px] rounded-2xl drop-shadow-lg">Husk sikkerhet!</button>
     </div>
     <div className="w-1/2 px-2 mb-4">
-        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full">Prosjekter</button>
+        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full font-mono text-[25px] rounded-2xl drop-shadow-lg">Prosjekter</button>
     </div>
     </section>
     </>
