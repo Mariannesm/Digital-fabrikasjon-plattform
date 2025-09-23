849a19805e20e21afefaffde01a385de3a95b65e
Mariannesm <marmyr6@hotmail.com>
2025-09-23 21:40:13 +0200

    Update

--- src/pages/landingpage/LandingPage.jsx
+++ src/pages/landingpage/LandingPage.jsx
@@ -5,20 +5,19 @@ import { Route, Routes } from "react-router-dom";
 function LandingPage() {
   return (
     <>
-    <section className="flex flex-wrap justify-center w-200 h-100 m-auto">
-    <div className="w-1/2 px-2 mb-4">
-        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full font-mono text-[25px] rounded-2xl drop-shadow-lg">Bruk teknologiene</button>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+    <h1 className="text-7xl font-bold text-[#E69138] mb-20">SmartMaking</h1>
+    <div className="relative">
+      <select className="text-lg w-64 px-5 py-3 rounded-lg border border-gray-200 bg-gray-50 text-gray-800 shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400">
+        <option>Velg område</option>
+        <option>Høgskolen i Østfold</option>
+        <option>OsloMet</option>
+      </select>
     </div>
-    <div className="w-1/2 px-2 mb-4">
-        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full font-mono text-[25px] rounded-2xl drop-shadow-lg">Lær om bærekraftig bruk</button>
-    </div>
-    <div className="w-1/2 px-2 mb-4">
-        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full font-mono text-[25px] rounded-2xl drop-shadow-lg">Husk sikkerhet!</button>
-    </div>
-    <div className="w-1/2 px-2 mb-4">
-        <button className="bg-[#E69138] text-black p-4 rounded w-full h-full font-mono text-[25px] rounded-2xl drop-shadow-lg">Prosjekter</button>
-    </div>
-    </section>
+    <button className="w-64 px-5 py-3 rounded-lg bg-[#E69138] text-[#663500] text-xl font-bold shadow-ml focus:outline-none focus:ring-2 focus:ring-orange-400 mt-10 hover:bg-orange-400">
+      Gå videre
+    </button>
+  </div>
     </>
   )
 }
