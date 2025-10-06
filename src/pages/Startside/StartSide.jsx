cc06c62378716285237e06aaf7bd453cacfcc46e
Mariannesm <marmyr6@hotmail.com>
2025-10-06 22:45:52 +0200

    Update

--- src/pages/Startside/StartSide.jsx
+++ src/pages/Startside/StartSide.jsx
@@ -4,7 +4,14 @@ import './startside.css'
 function LandingPage() {
   return (
     <>
-    <h1>Landingsside</h1>
+    <h1 className="text-9xl font-bold text-[#E69138] mb-20">SmartMaking</h1>
+    <section>
+      <button>BRUK TEKNOLOGIENE</button>
+      <button>LÆR OM BÆREKRAFTIG BRUK</button>
+      <button>HUSK SIKKERHET!</button>
+      <button>PROSJEKTER</button>
+    </section>
+    <button>Chatbot</button>
     </>
   )
 }
