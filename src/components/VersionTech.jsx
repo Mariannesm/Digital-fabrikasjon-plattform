ff84a78e62a48efe92194587d0f7b84635625266
Mariannesm <marmyr6@hotmail.com>
2025-12-15 19:19:29 +0100

    Endret i card componentet

--- src/components/VersionTech.jsx
+++ src/components/VersionTech.jsx
@@ -2,28 +2,23 @@ import React from "react";
 import Printer from "../assets/ikoner/Printer.png"
 
 
-function VersionTech() {
+function VersionTech({data}) {
   return (
+    <div className="flex flex-col w-90 h- rounded-xl border border-[#488B90] bg-white shadow-sm overflow-hidden">
 
+      <div className="h-50 bg-[#488B90] flex items-center justify-center">
+        <img className="pb-2 pt-2 w-25 h-45" src={Printer} alt="Bilde av Prusa MK4S"></img>
+      </div>
 
-  <div className="flex flex-col w-90 h- rounded-xl border border-[#488B90] bg-white shadow-sm">
+      <div className="text-left p-7">
+        <h3 className="font-semibold pb-5">{data?.title}</h3>
+        <p>{data?.description}</p>
+      </div>
 
-  <div className="h-50  rounded-t-xl bg-[#488B90] flex items-center justify-center">
-    <img className="pb-2 pt-2 w-25 h-45" src={Printer} alt="Bilde av Prusa MK4S"></img>
-  </div>
-
-  <div className="text-left p-7">
-  <h3 className="font-semibold pb-5">Original Prusa MK4S</h3>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-  </div>
-
-    <div className="mt-4 align-items-end gap-20 flex-col flex">
-     <button className="flex-0 bg-[#214C50] rounded-2xl px-5 py-3 mb-4 text-lg text-white font-semibold shadow-md hover:bg-[#488B90]">Velg</button>
+      <div className="mt-auto flex flex-col p-3">
+        <button className="self-end bg-[#214C50] rounded-2xl px-5 py-3 mb-4 text-lg text-white font-semibold shadow-md hover:bg-[#488B90]">Velg</button>
+      </div>
     </div>
-</div>
 
   );
 }
--- src/pages/SelectedTechnology/SelectedTechnology.jsx
+++ src/pages/SelectedTechnology/SelectedTechnology.jsx
@@ -3,14 +3,19 @@ import Header from "../../components/Header";
 import MainWrapper from "../../components/MainWrapper";
 import VersionTech from "../../components/VersionTech";
 
-
-
 function SelectedTechnology() {
+
+  const printers = [
+    { title: "Original Prusa MK4S", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+    { title: "Original Prusa MK2", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+
+  ]
+
   return (
     <MainWrapper classNames="bg-[#FFFCF8]">
       {/* HEADER */}
       <Header title="3D-PRINTING" showSelectInstitution={false} />
-  
+
 
       {/* Erstattes med API-kall 
       title
@@ -23,12 +28,13 @@ function SelectedTechnology() {
 
       <h2 className="font-bold text-xl">Velg printeren du skal bruke</h2>
 
-
-      <div className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2 place-items-center">
-<VersionTech></VersionTech>
+      <div className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2 place-items-center items-stretch">
+        {printers.map((card) => {
+          return <VersionTech data={card} />
+        })}
       </div>
 
-      
+
     </MainWrapper>
   );
 }
