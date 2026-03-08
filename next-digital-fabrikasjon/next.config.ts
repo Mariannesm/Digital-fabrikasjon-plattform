dc246bc45dafea78d022f47e0dd3cf65a3f9260d
Mathias <mattis417@hotmail.com>
2026-03-08 14:29:16 +0100

    fix: fjern @schemas-alias og server/schemaTypes fra webpack/tsconfig

--- next-digital-fabrikasjon/next.config.ts
+++ next-digital-fabrikasjon/next.config.ts
@@ -1,21 +1,5 @@
 import type { NextConfig } from 'next'
-import path from 'path'
 
-const schemasPath = path.resolve(__dirname, '../server/schemaTypes')
-
-const nextConfig: NextConfig = {
-  turbopack: {
-    resolveAlias: {
-      '@schemas': schemasPath,
-    },
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@schemas': schemasPath,
-    }
-    return config
-  },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
